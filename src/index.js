c4217f5a76b67b23e30a91c9092c18740dba39ea
KotoJeep <omaigader@gmail.co>
2021-08-15 11:17:34 +0700

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,36 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+  let code = { 10: '.', 11: '-' },
+    arr = expr.match(/.{10}/g),
+    res = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != '**********') arr[i] = +arr[i];
+  }
+  
+  let newArr = arr.map((a) => (a.toString().match(/.{2}/g)));
+  
+  for (let j = 0; j < newArr.length; j++) {
+    let jj = newArr[j];
+    for (let i = 0; i < jj.length; i++) {
+      for (const [key, value] of Object.entries(code)) {
+        if (jj[i] == key) {
+          jj[i] = value;
+        }
+      }
+    }
+    res.push(jj.join(''));
+  }
+  
+  for (let i = 0; i < res.length; i++) {
+    for (const [key, value] of Object.entries(MORSE_TABLE)) {
+      if (res[i] == key) {
+        res[i] = value;
+      }
+    }
+  }
+return res.join('').replace(/\b\*\*\*\*\*\*\*\*\*\*\b/g, ' ')
 }
 
 module.exports = {
